3303be646560323bafd26a3f7bf008a049b93bfb
michael <38920570+msolanoimagina@users.noreply.github.com>
2019-08-09 13:03:08 -0500

    +Add: filter by "userId" and "name" in Field repository +Update: getter and setter of field value, to save mainImage

--- Repositories/Eloquent/EloquentFieldRepository.php
+++ Repositories/Eloquent/EloquentFieldRepository.php
@@ -7,12 +7,12 @@ use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 
 class EloquentFieldRepository extends EloquentBaseRepository implements FieldRepository
 {
-  
+
   public function getItemsBy($params = false)
   {
     /*== initialize query ==*/
     $query = $this->model->query();
-    
+
     /*== RELATIONSHIPS ==*/
     if (in_array('*', $params->include)) {//If Request all relationships
       $query->with([]);
@@ -22,11 +22,11 @@ class EloquentFieldRepository extends EloquentBaseRepository implements FieldRep
         $includeDefault = array_merge($includeDefault, $params->include);
       $query->with($includeDefault);//Add Relationships to query
     }
-    
+
     /*== FILTERS ==*/
     if (isset($params->filter)) {
       $filter = $params->filter;//Short filter
-      
+
       //Filter by date
       if (isset($filter->date)) {
         $date = $filter->date;//Short filter date
@@ -36,7 +36,17 @@ class EloquentFieldRepository extends EloquentBaseRepository implements FieldRep
         if (isset($date->to))//to a date
           $query->whereDate($date->field, '<=', $date->to);
       }
-      
+
+      //By User ID
+      if (isset($filter->userId)) {
+        $query->where('user_id', $filter->userId);
+      }
+
+      //By Name
+      if (isset($filter->name)) {
+        $query->where('name', $filter->name);
+      }
+
       //Order by
       if (isset($filter->order)) {
         $orderByField = $filter->order->field ?? 'created_at';//Default field
@@ -44,11 +54,11 @@ class EloquentFieldRepository extends EloquentBaseRepository implements FieldRep
         $query->orderBy($orderByField, $orderWay);//Add order to query
       }
     }
-    
+
     /*== FIELDS ==*/
     if (isset($params->fields) && count($params->fields))
       $query->select($params->fields);
-    
+
     /*== REQUEST ==*/
     if (isset($params->page) && $params->page) {
       return $query->paginate($params->take);
@@ -57,12 +67,12 @@ class EloquentFieldRepository extends EloquentBaseRepository implements FieldRep
       return $query->get();
     }
   }
-  
+
   public function getItem($criteria, $params = false)
   {
     //Initialize query
     $query = $this->model->query();
-    
+
     /*== RELATIONSHIPS ==*/
     if (in_array('*', $params->include)) {//If Request all relationships
       $query->with([]);
@@ -72,84 +82,81 @@ class EloquentFieldRepository extends EloquentBaseRepository implements FieldRep
         $includeDefault = array_merge($includeDefault, $params->include);
       $query->with($includeDefault);//Add Relationships to query
     }
-    
+
     /*== FILTER ==*/
     if (isset($params->filter)) {
       $filter = $params->filter;
-      
+
       if (isset($filter->field))//Filter by specific field
         $field = $filter->field;
     }
-    
+
     /*== FIELDS ==*/
     if (isset($params->fields) && count($params->fields))
       $query->select($params->fields);
-    
+
     /*== REQUEST ==*/
     return $query->where($field ?? 'id', $criteria)->first();
   }
-  
+
   public function create($data)
   {
     //find if exist with the same name
-    $model = $this->findByAttributes(["name" => $data["name"]]);
-    
-    if(!$model){
+    $model = $this->findByAttributes(["name" => $data["name"], 'user_id' => $data["user_id"]]);
+
+    if (!$model) {
       $field = $this->model->create($data);
-  
       $newData = $field->toArray();
       return $field;
-    }else{
-      return $this->updateBy($data["name"],$data,(object)["filter" => (object)["field" => "name"]]);
+    } else {
+      return $this->updateBy($model->id, $data, (object)["filter" => (object)["field" => "id"]]);
     }
-    
-    
   }
-  
+
   public function updateBy($criteria, $data, $params = false)
   {
     /*== initialize query ==*/
     $query = $this->model->query();
-    
+
     /*== FILTER ==*/
     if (isset($params->filter)) {
       $filter = $params->filter;
-      
+
       //Update by field
       if (isset($filter->field))
         $field = $filter->field;
     }
-    
+
     /*== REQUEST ==*/
     $model = $query->where($field ?? 'id', $criteria)->first();
-  
-    if($model) {
+
+    if ($model) {
       $oldData = $model->toArray();
       $model->update($data);
       $newData = $model->toArray();
     }
     return $model;
   }
-  
+
   public function deleteBy($criteria, $params = false)
   {
     /*== initialize query ==*/
     $query = $this->model->query();
-    
+
     /*== FILTER ==*/
     if (isset($params->filter)) {
       $filter = $params->filter;
-      
+
       if (isset($filter->field))//Where field
         $field = $filter->field;
     }
-    
+
     /*== REQUEST ==*/
     $model = $query->where($field ?? 'id', $criteria)->first();
-    if($model) {
+    if ($model) {
       $oldData = $model->toArray();
       $model->delete();
-    
+
     }
   }
 }
